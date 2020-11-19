e30a81c1ad7e1b6d0005926c35905cceded583c3
Kyle Karpack <karpack@adobe.com>
2020-11-18 16:14:42 -0800

    Format everything

--- src/templates/BlogIndex.js
+++ src/templates/BlogIndex.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import { Location } from '@reach/router'
-import qs from 'qs'
+import React from "react";
+import { graphql } from "gatsby";
+import { Location } from "@reach/router";
+import qs from "qs";
 
-import PageHeader from '../components/PageHeader'
-import PostSection from '../components/PostSection'
-import PostCategoriesNav from '../components/PostCategoriesNav'
-import Layout from '../components/Layout'
+import PageHeader from "../components/PageHeader";
+import PostSection from "../components/PostSection";
+import PostCategoriesNav from "../components/PostCategoriesNav";
+import Layout from "../components/Layout";
 
 /**
  * Filter posts by date. Feature dates will be fitered
@@ -14,10 +14,10 @@ import Layout from '../components/Layout'
  *
  * @param {posts} object
  */
-export const byDate = posts => {
-  const now = Date.now()
-  return posts.filter(post => Date.parse(post.date) <= now)
-}
+export const byDate = (posts) => {
+	const now = Date.now();
+	return posts.filter((post) => Date.parse(post.date) <= now);
+};
 
 /**
  * filter posts by category.
@@ -27,150 +27,153 @@ export const byDate = posts => {
  * @param {contentType} string
  */
 export const byCategory = (posts, title, contentType) => {
-  const isCategory = contentType === 'postCategories'
-  const byCategory = post =>
-    post.categories &&
-    post.categories.filter(cat => cat.category === title).length
-  return isCategory ? posts.filter(byCategory) : posts
-}
+	const isCategory = contentType === "postCategories";
+	const byCategory = (post) =>
+		post.categories &&
+		post.categories.filter((cat) => cat.category === title).length;
+	return isCategory ? posts.filter(byCategory) : posts;
+};
 
 // Export Template for use in CMS preview
 export const BlogIndexTemplate = ({
-  title,
-  subtitle,
-  featuredImage,
-  posts = [],
-  postCategories = [],
-  enableSearch = true,
-  contentType
+	title,
+	subtitle,
+	featuredImage,
+	posts = [],
+	postCategories = [],
+	enableSearch = true,
+	contentType
 }) => (
-  <Location>
-    {({ location }) => {
-      let filteredPosts =
-        posts && !!posts.length
-          ? byCategory(byDate(posts), title, contentType)
-          : []
+	<Location>
+		{({ location }) => {
+			let filteredPosts =
+				posts && !!posts.length
+					? byCategory(byDate(posts), title, contentType)
+					: [];
 
-      let queryObj = location.search.replace('?', '')
-      queryObj = qs.parse(queryObj)
+			let queryObj = location.search.replace("?", "");
+			queryObj = qs.parse(queryObj);
 
-      if (enableSearch && queryObj.s) {
-        const searchTerm = queryObj.s.toLowerCase()
-        filteredPosts = filteredPosts.filter(post =>
-          post.frontmatter.title.toLowerCase().includes(searchTerm)
-        )
-      }
+			if (enableSearch && queryObj.s) {
+				const searchTerm = queryObj.s.toLowerCase();
+				filteredPosts = filteredPosts.filter((post) =>
+					post.frontmatter.title.toLowerCase().includes(searchTerm)
+				);
+			}
 
-      return (
-        <main className="Blog">
-          <PageHeader
-            title={title}
-            subtitle={subtitle}
-            backgroundImage={featuredImage}
-          />
+			return (
+				<main className="Blog">
+					<PageHeader
+						title={title}
+						subtitle={subtitle}
+						backgroundImage={featuredImage}
+					/>
 
-          {!!postCategories.length && (
-            <section className="section thin">
-              <div className="container">
-                <PostCategoriesNav enableSearch categories={postCategories} />
-              </div>
-            </section>
-          )}
+					{!!postCategories.length && (
+						<section className="section thin">
+							<div className="container">
+								<PostCategoriesNav
+									enableSearch
+									categories={postCategories}
+								/>
+							</div>
+						</section>
+					)}
 
-          {!!posts.length && (
-            <section className="section">
-              <div className="container">
-                <PostSection posts={filteredPosts} />
-              </div>
-            </section>
-          )}
-        </main>
-      )
-    }}
-  </Location>
-)
+					{!!posts.length && (
+						<section className="section">
+							<div className="container">
+								<PostSection posts={filteredPosts} />
+							</div>
+						</section>
+					)}
+				</main>
+			);
+		}}
+	</Location>
+);
 
 // Export Default BlogIndex for front-end
 const BlogIndex = ({ data: { page, posts, postCategories } }) => (
-  <Layout
-    meta={page.frontmatter.meta || false}
-    title={page.frontmatter.title || false}
-  >
-    <BlogIndexTemplate
-      {...page}
-      {...page.fields}
-      {...page.frontmatter}
-      posts={posts.edges.map(post => ({
-        ...post.node,
-        ...post.node.frontmatter,
-        ...post.node.fields
-      }))}
-      postCategories={postCategories.edges.map(post => ({
-        ...post.node,
-        ...post.node.frontmatter,
-        ...post.node.fields
-      }))}
-    />
-  </Layout>
-)
+	<Layout
+		meta={page.frontmatter.meta || false}
+		title={page.frontmatter.title || false}
+	>
+		<BlogIndexTemplate
+			{...page}
+			{...page.fields}
+			{...page.frontmatter}
+			posts={posts.edges.map((post) => ({
+				...post.node,
+				...post.node.frontmatter,
+				...post.node.fields
+			}))}
+			postCategories={postCategories.edges.map((post) => ({
+				...post.node,
+				...post.node.frontmatter,
+				...post.node.fields
+			}))}
+		/>
+	</Layout>
+);
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
-  ## Query for BlogIndex data
-  ## Use GraphiQL interface (http://localhost:8000/___graphql)
-  ## $id is processed via gatsby-node.js
-  ## query name must be unique to this file
-  query BlogIndex($id: String!) {
-    page: markdownRemark(id: { eq: $id }) {
-      ...Meta
-      fields {
-        contentType
-      }
-      frontmatter {
-        title
-        excerpt
-        template
-        subtitle
-        featuredImage
-      }
-    }
+	## Query for BlogIndex data
+	## Use GraphiQL interface (http://localhost:8000/___graphql)
+	## $id is processed via gatsby-node.js
+	## query name must be unique to this file
+	query BlogIndex($id: String!) {
+		page: markdownRemark(id: { eq: $id }) {
+			...Meta
+			fields {
+				contentType
+			}
+			frontmatter {
+				title
+				excerpt
+				template
+				subtitle
+				featuredImage
+			}
+		}
 
-    posts: allMarkdownRemark(
-      filter: { fields: { contentType: { eq: "posts" } } }
-      sort: { order: DESC, fields: [frontmatter___date] }
-    ) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            date
-            categories {
-              category
-            }
-            featuredImage
-          }
-        }
-      }
-    }
-    postCategories: allMarkdownRemark(
-      filter: { fields: { contentType: { eq: "postCategories" } } }
-      sort: { order: ASC, fields: [frontmatter___title] }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-          }
-        }
-      }
-    }
-  }
-`
+		posts: allMarkdownRemark(
+			filter: { fields: { contentType: { eq: "posts" } } }
+			sort: { order: DESC, fields: [frontmatter___date] }
+		) {
+			edges {
+				node {
+					excerpt
+					fields {
+						slug
+					}
+					frontmatter {
+						title
+						date
+						categories {
+							category
+						}
+						featuredImage
+					}
+				}
+			}
+		}
+		postCategories: allMarkdownRemark(
+			filter: { fields: { contentType: { eq: "postCategories" } } }
+			sort: { order: ASC, fields: [frontmatter___title] }
+		) {
+			edges {
+				node {
+					fields {
+						slug
+					}
+					frontmatter {
+						title
+					}
+				}
+			}
+		}
+	}
+`;
