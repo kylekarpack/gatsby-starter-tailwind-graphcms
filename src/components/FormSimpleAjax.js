516288b1613e4c5c8aa1fa48892da7f2d0bb975d
Kyle Karpack <kylekarpack@gmail.com>
2020-11-22 23:04:53 -0800

    Contact fixes

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -72,49 +72,16 @@ class Form extends React.Component {
 					{this.state.alert && (
 						<div className="Form--Alert">{this.state.alert}</div>
 					)}
-					<div className="Form--Group">
-						<label className="Form--Label">
-							<input
-								className="Form--Input Form--InputText"
-								type="text"
-								placeholder="Firstname"
-								name="firstname"
-								required
-							/>
-							<span>Firstname</span>
-						</label>
-						<label className="Form--Label">
-							<input
-								className="Form--Input Form--InputText"
-								type="text"
-								placeholder="Lastname"
-								name="lastname"
-								required
-							/>
-							<span>Lastname</span>
-						</label>
-					</div>
-					<fieldset>
-						<label className="Form--Label Form--Radio">
-							<input
-								className="Form--RadioInput"
-								type="radio"
-								name="gender"
-								value="male"
-								defaultChecked
-							/>
-							<span>Male</span>
-						</label>
-						<label className="Form--Label Form--Radio">
-							<input
-								className="Form--RadioInput"
-								type="radio"
-								name="gender"
-								value="female"
-							/>
-							<span>Female</span>
-						</label>
-					</fieldset>
+					<label className="Form--Label">
+						<input
+							className="Form--Input Form--InputText"
+							type="text"
+							placeholder="Name"
+							name="name"
+							required
+						/>
+						<span>Name</span>
+					</label>
 					<label className="Form--Label">
 						<input
 							className="Form--Input Form--InputText"
@@ -125,21 +92,6 @@ class Form extends React.Component {
 						/>
 						<span>Email address</span>
 					</label>
-					<label className="Form--Label has-arrow">
-						<select
-							className="Form--Input Form--Select"
-							name="type"
-							defaultValue="Type of Enquiry"
-							required
-						>
-							<option disabled hidden>
-								Type of Enquiry
-							</option>
-							<option>Need to know more</option>
-							<option>Found a bug</option>
-							<option>Want to say hello</option>
-						</select>
-					</label>
 					<label className="Form--Label">
 						<textarea
 							className="Form--Input Form--Textarea Form--InputText"
@@ -149,14 +101,6 @@ class Form extends React.Component {
 							required
 						/>
 						<span>Message</span>
-					</label>
-					<label className="Form--Label Form-Checkbox">
-						<input
-							className="Form--Input Form--Textarea Form--CheckboxInput"
-							name="newsletter"
-							type="checkbox"
-						/>
-						<span>Get news updates</span>
 					</label>
 					<div
 						className="g-recaptcha"
@@ -169,7 +113,7 @@ class Form extends React.Component {
 					<input
 						className="Button Form--SubmitButton"
 						type="submit"
-						value="Enquire"
+						value="Send"
 						disabled={this.state.disabled}
 					/>
 				</form>
