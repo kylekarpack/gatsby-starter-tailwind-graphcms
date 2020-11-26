4fb0d9e003f14bd17b9657462d94dd685c382d2e
Kyle Karpack <kylekarpack@gmail.com>
2020-11-25 19:43:19 -0800

    Portfolio added

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,83 @@
+import { graphql, useStaticQuery } from "gatsby";
+import BackgroundImage from "gatsby-background-image";
+import React from "react";
+import "./PagePreview.css";
+
+const Excerpt = ({ text, minLength = 100 }) => {
+	text = text || "";
+	let processedExcerpt = "";
+	const split = text.split(".");
+	for (let i = 0; i < split.length; i++) {
+		processedExcerpt += split[i];
+		processedExcerpt += ".";
+		if (processedExcerpt.length > minLength) {
+			break;
+		}
+	}
+	return <>{processedExcerpt}</>;
+};
+
+const Page = ({ page, excerpt, height }) => {
+	height = height || "200px";
+	return (
+		<a className="Page" href={page.fields.slug}>
+			<div className="Page--main">
+				<BackgroundImage
+					style={{ height }}
+					fluid={page.frontmatter?.featuredImage?.childImageSharp?.fluid}
+				/>
+				<div className="caption">
+					<div className="name">{page.frontmatter.title}</div>
+					{page.frontmatter.subtitle && (
+						<div className="subtitle">{page.frontmatter.subtitle}</div>
+					)}
+				</div>
+			</div>
+			{excerpt && (
+				<p className="Excerpt">
+					<Excerpt text={page.excerpt} />
+				</p>
+			)}
+		</a>
+	);
+};
+
+const Portfolio = ({ type, excerpt, height }) => {
+	let pages = useStaticQuery(graphql`
+		query {
+			allMdx(
+				sort: { fields: frontmatter___order }
+				filter: { slug: { glob: "pages/portfolio/*" } }
+			) {
+				nodes {
+					fields {
+						slug
+					}
+					excerpt(pruneLength: 1000)
+					frontmatter {
+						title
+						subtitle
+						categories
+						featuredImage {
+							childImageSharp {
+								fluid(maxWidth: 400) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	`).allMdx.nodes;
+
+	return (
+		<div bp="grid 6@md 3@lg">
+			{pages.map((page, i) => (
+				<Page page={page} excerpt={excerpt} height={height} key={i} />
+			))}
+		</div>
+	);
+};
+
+export default Portfolio;
