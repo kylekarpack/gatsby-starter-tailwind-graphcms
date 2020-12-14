5214ce35ca6e2550e7f8538570c180158520202f
Kyle Karpack <kylekarpack@gmail.com>
2020-12-14 08:14:57 -0800

    Start tailwind

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -52,24 +52,191 @@ export const Navigation = (props) => {
 	};
 
 	const NavLink = ({ to, className, children, ...props }) => {
-		return <Link
-			to={to}
-			className={`NavLink ${
-				to === state.currentPath ? "active" : ""
-			} ${className || ""}`}
-			onClick={handleLinkClick}
-			onKeyDown={handleLinkKeyDown}
-			tabIndex={0}
-			aria-label="Navigation"
-			role="button"
-			{...props}
-		>
-			{children}
-		</Link>
+		return (
+			<Link
+				to={to}
+				className={`NavLink ${to === state.currentPath ? "active" : ""} ${
+					className || ""
+				}`}
+				onClick={handleLinkClick}
+				onKeyDown={handleLinkKeyDown}
+				tabIndex={0}
+				aria-label="Navigation"
+				role="button"
+				{...props}
+			>
+				{children}
+			</Link>
+		);
 	};
 
 	const { active } = state;
 	const { subNav } = props;
+
+	return (
+		<>
+			<nav className="bg-green-700">
+				<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+					<div className="relative flex items-center justify-between h-16">
+						<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+							{/* Mobile menu button*/}
+							<button
+								className="inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+								aria-expanded="false"
+							>
+								<span className="sr-only">Open main menu</span>
+								{/* Icon when menu is closed. */}
+								{/*
+				Heroicon name: menu
+	
+				Menu open: "hidden", Menu closed: "block"
+			*/}
+								<svg
+									className="block h-6 w-6"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									aria-hidden="true"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={2}
+										d="M4 6h16M4 12h16M4 18h16"
+									/>
+								</svg>
+								{/* Icon when menu is open. */}
+								{/*
+				Heroicon name: x
+	
+				Menu open: "block", Menu closed: "hidden"
+			*/}
+								<svg
+									className="hidden h-6 w-6"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									aria-hidden="true"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={2}
+										d="M6 18L18 6M6 6l12 12"
+									/>
+								</svg>
+							</button>
+						</div>
+						<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+							<div className="flex-shrink-0 flex items-center">
+								<a href="/">
+									<img
+										className="block lg:hidden h-8 w-auto"
+										src="/favicon.png"
+										alt="Workflow"
+									/>
+									<img
+										className="hidden lg:block h-8 w-auto"
+										src="/images/logo.svg"
+										alt="Workflow"
+									/>
+								</a>
+							</div>
+							<div className="hidden sm:block sm:ml-6">
+								<div className="flex space-x-4">
+									{/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+									<NavLink
+										to="/portfolio"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									>
+										Portfolio
+									</NavLink>
+									<NavLink
+										to="/tools-skills"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									>
+										Tools &amp; Skills
+									</NavLink>
+									<NavLink
+										to="/team/"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									>
+										Team
+									</NavLink>
+									<NavLink
+										to="/careers/"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									>
+										Careers
+									</NavLink>
+									<NavLink
+										to="/community-service/"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									>
+										Community Service
+									</NavLink>
+									<NavLink
+										to="/contact/"
+										className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+									>
+										Contact
+									</NavLink>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				{/*
+			Mobile menu, toggle classes based on menu state.
+	
+			Menu open: "block", Menu closed: "hidden"
+		*/}
+				<div className="hidden sm:hidden">
+					<div className="px-2 pt-2 pb-3 space-y-1">
+						{/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+						<NavLink
+							to="/portfolio"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						>
+							Portfolio
+						</NavLink>
+						<NavLink
+							to="/tools-skills"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						>
+							Tools &amp; Skills
+						</NavLink>
+						<NavLink
+							to="/team/"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						>
+							Team
+						</NavLink>
+						<NavLink
+							to="/careers/"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						>
+							Careers
+						</NavLink>
+						<NavLink
+							to="/community-service/"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						>
+							Community Service
+						</NavLink>
+						<NavLink
+							to="/contact/"
+							className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						>
+							Contact
+						</NavLink>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 
 	return (
 		<nav className={`Nav ${active ? "Nav-active" : ""}`}>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+		extend: {},
+		colors: {
+			green: "#3c5e31"
+		}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
