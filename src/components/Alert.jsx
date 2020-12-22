48e3f050b5cd6dd7ce2b619b81ebdad77a6f3943
Kyle Karpack <kylekarpack@gmail.com>
2020-12-21 21:13:23 -0800

    Fix deployed issues

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { AlertTriangle, Info, Check, XCircle } from "react-feather";
+import { AlertTriangle, Check, Info, XCircle } from "react-feather";
 
-const Alert = ({ color, children }) => {
+const Alert = ({ type, children }) => {
+	let className = "";
+	let icon = "";
+	switch (type) {
+		case "warning":
+			className = "bg-yellow-100 text-yellow-900";
+			icon = <AlertTriangle className="w-24 h-12" />;
+			break;
+		case "danger":
+			className = "bg-red-100 text-red-900";
+			icon = <XCircle className="w-24 h-12" />;
+			break;
+		case "success":
+			className = "bg-green-100 text-green-900";
+			icon = <Check className="w-24 h-12" />;
+			break;
+		default:
+			// info
+			className = "bg-blue-100 text-blue-900";
+			icon = <Info className="w-24 h-12" />;
+	}
 	return (
 		<div
-			className={`bg-${color}-100 text-${color}-900 px-6 py-4 border-0 rounded relative mb-8 flex items-center gap-4`}
+			className={`${className} px-6 py-4 border-0 rounded relative mb-8 flex items-center gap-4`}
 		>
-			{color === "yellow" && <AlertTriangle className="w-24 h-12" />}
-			{color === "green" && <Check className="w-24 h-12" />}
-			{color === "blue" && <Info className="w-24 h-12" />}
-			{color === "red" && <XCircle className="w-24 h-12" />}
+			{icon}
 			<span className="inline-block align-middle mr-8">{children}</span>
 		</div>
 	);
--- src/components/FileMail.jsx
+++ src/components/FileMail.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Helmet from "react-helmet";
 
 const FileMail = ({ companyId }) => {
+	// clear out filemail when component is removed
+	useEffect(() => {
+		return () => {
+			const fileMails = document.querySelectorAll(
+				"script[src^='//www.filemail.com/']"
+			);
+			[...fileMails].forEach((el) => el.remove());
+		};
+	}, []);
 	return (
 		<div>
 			<div id="FilemailUploadWrap"></div>
 			<Helmet defer={false}>
+				<script>console.warn(window)</script>
 				<script
 					type="text/javascript"
-					src={`https://www.filemail.com/js/widgets/uploadIntegrator2.js?companyid=${companyId}&wrapId=FilemailUploadWrap`}
+					src={`//www.filemail.com/js/widgets/uploadIntegrator2.js?companyid=${companyId}&wrapId=FilemailUploadWrap`}
 				></script>
 			</Helmet>
 		</div>
