2d82e69cf373483e903a35fe651788962c913887
Kyle Karpack <kylekarpack@gmail.com>
2021-02-01 22:00:38 -0800

    Page preview

--- src/components/PagePreview.jsx
+++ src/components/PagePreview.jsx
@@ -1,9 +1,9 @@
 import { Link } from "gatsby";
 import BackgroundImage from "gatsby-background-image";
 import Image from "gatsby-image";
-import React from "react";
+import React, { useState } from "react";
 
-export const Excerpt = ({ text, minLength = 100 }) => {
+const Excerpt = ({ text, minLength = 100 }) => {
 	text = text || "";
 	let processedExcerpt = "";
 	const split = text.split(".");
@@ -17,7 +17,7 @@ export const Excerpt = ({ text, minLength = 100 }) => {
 	return <>{processedExcerpt}</>;
 };
 
-export const Page = ({
+const Page = ({
 	page,
 	excerpt,
 	overlay,
@@ -37,7 +37,7 @@ export const Page = ({
 		>
 			{masonry ? (
 				<div className="relative">
-					<Image fluid={image} style={{minHeight: "100px"}} />
+					<Image fluid={image} style={{ minHeight: "100px" }} />
 					<div className="absolute opacity-0 hover:opacity-100 transition-opacity duration-300 px-4 left-0 right-0 top-0 bottom-0 bg-black bg-opacity-60 text-center flex justify-center items-center">
 						<span className="font-bold text-xl text-white px-4 leading-5">
 							{page.title}
@@ -95,12 +95,45 @@ export const Page = ({
 	);
 };
 
-const PagePreview = ({ options, items }) => {
-	console.warn(options);
+const FilterMenu = ({ items, setPreviewItems }) => {
+	const [filter, setFilter] = useState("All");
+	const updateFilter = (value) => {
+		const updatedItems =
+			!value || value === "All" || value === "*"
+				? items
+				: items.filter((el) =>
+						el.categories?.find((c) => c.title === value)
+				  );
+		setFilter(value);
+		setPreviewItems(updatedItems);
+	};
 
-	// if (type) {
-	// 	items = items.filter((el) => el.frontmatter?.type === type);
-	// }
+	let filters = [
+		...new Set(items.flatMap((el) => el.categories.map((c) => c.title)))
+	];
+	filters.sort((a, b) => a.localeCompare(b));
+	filters.unshift("All");
+
+	return (
+		<div className="flex justify-center flex-wrap mb-12">
+			{filters.map((el, i) => (
+				<button
+					key={i}
+					type="button"
+					className={`inline-block px-1 py-1 mr-1 mb-1 text-xs text-center text-white transition bg-gray-900 rounded shadow ripple hover:shadow-lg hover:bg-primary focus:outline-none ${
+						filter === el ? "bg-primary" : ""
+					}`}
+					onClick={() => updateFilter(el)}
+				>
+					{el}
+				</button>
+			))}
+		</div>
+	);
+};
+
+const PagePreview = ({ options, items }) => {
+	const [previewItems, setPreviewItems] = useState(items);
 
 	const style = options?.masonry
 		? {
@@ -113,21 +146,29 @@ const PagePreview = ({ options, items }) => {
 		: {};
 
 	return (
-		<div
-			style={style}
-			className="grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8"
-		>
-			{items.map((page, i) => (
-				<Page
-					page={page}
-					excerpt={options?.excerpt !== false}
-					overlay={options?.overlay}
-					height={options?.height}
-					masonry={options?.masonry}
-					className={`Preview ${options?.masonry ? "inline-block mb-4" : ""}`}
-					key={i}
-				/>
-			))}
+		<div>
+			{options?.filterable && (
+				<FilterMenu items={items} setPreviewItems={setPreviewItems} />
+			)}
+			<div
+				style={style}
+				className="grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8"
+			>
+				{previewItems.map((page, i) => (
+					<Page
+						page={page}
+						excerpt={options?.excerpt !== false}
+						overlay={options?.overlay}
+						height={options?.height}
+						masonry={options?.masonry}
+						readMore={options?.readMore}
+						className={`Preview ${
+							options?.masonry ? "inline-block mb-4" : ""
+						}`}
+						key={i}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
